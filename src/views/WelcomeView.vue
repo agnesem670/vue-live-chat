e17b57100f896dad6653b5be945498f0032de5fc
agnesem670 <agnesem670@gmail.com>
2023-11-14 12:28:50 +0100

    Project setup

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome container">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'WelcomeView'
+}
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
